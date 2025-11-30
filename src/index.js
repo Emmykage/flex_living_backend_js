29af865f0d0dc68a5ac7ee4e066be5f3835bf54a
morris menanya <emmiemenz@gmail.com>
2025-11-30 11:59:03 +0100

    to view adnd debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import fs from "fs"
+import path from "path"
 import reviewRouter from './routes/reviewRoutes.js';
 import { ErrorHandler } from './middleware/errorHandler.js';
 import cors from 'cors';
@@ -15,6 +16,12 @@ app.use('/api/reviews', reviewRouter);
 app.use(ErrorHandler.handle);
 
 
+console.log("Current working directory:", process.cwd());
+console.log("Files in root:");
+console.log(fs.readdirSync(process.cwd()));
+
+console.log("Files in src:");
+console.log(fs.readdirSync(path.join(process.cwd(), "src")));
 const PORT = process.env.PORT || 500
 app.listen(PORT, ()=> {
     console.log(`Server running on Port ${PORT}`)
